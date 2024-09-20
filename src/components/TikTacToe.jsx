8472dc9754d1b0957b25a061ab0b6721bdc767a9
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2024-09-20 13:34:34 +0530

    code components (Board.jsx , Tile.jsx , TicTacToe.jsx)

--- src/components/TikTacToe.jsx
+++ src/components/TikTacToe.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import Board from './Board';
 
+const PLAYER_X = 'X';
+const PLAYER_O = 'O';
+
 function TicTacToe() {
+  const [tiles, setTiles] = useState(Array(9).fill(null));
+  const { playerTurn, setPlayerTurn } = useState(PLAYER_X);
+
+  const handelTileClick = (index) => {
+    console.log(index);
+  };
+
   return (
     <div>
       Tic Tac Toe
-      <Board />
+      <Board tiles={tiles} onTileClick={handelTileClick} />
     </div>
   );
 }
